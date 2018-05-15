4bbe7069c6e62e7c020816905c22d14d1a7a2d26
antcin <ciniglioantonio@gmail.com>
2018-05-15 12:00:39 +0100

    pass first test for App.js

---
+++ src/components/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App';
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('App', () => {
+  const app = shallow(<App />);
+
+  it('renders properly', () => {
+    expect(app).toMatchSnapshot();
+  });
+});
