168a145bfb3a0a363a3d9725bf41fc8d8ec0718b
antcin <ciniglioantonio@gmail.com>
2018-05-25 23:39:52 +0100

    update README

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -18,7 +18,7 @@ export class Wallet extends Component {
   render() {
     return (
       <div>
-        <h3 className='balance'>Wallet balance: {this.props.balance}</h3>
+        <h3 className='balance'>Wallet balance: £{this.props.balance}</h3>
         <br />
         <input className='input-wallet' onChange={this.updateBalance} />
         <button className='btn-deposit' onClick={this.deposit}>Deposit</button>
