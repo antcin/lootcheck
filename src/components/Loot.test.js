a5b6912cf509168f4ef0385ef05021f0b1dfb378
antcin <ciniglioantonio@gmail.com>
2018-05-25 18:15:33 +0100

    pass test

--- src/components/Loot.test.js
+++ src/components/Loot.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Enzyme from 'enzyme';
 import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { Loot } from './Loot'; //importing the unconnected version of Loot as it's going to use Redux
